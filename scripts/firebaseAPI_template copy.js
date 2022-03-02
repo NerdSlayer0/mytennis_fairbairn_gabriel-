ca9db9d290c5ccd3e0922a3eee778f805c6b4a6c
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2022-03-01 23:52:38 -0800

    demo 9 steps

--- scripts/firebaseAPI_template copy.js
+++ scripts/firebaseAPI_template copy.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyABeXnpvAidXNyWifGQ7saR-BvAkgdDY9w",
+    authDomain: "fir-comp1800-f0f91.firebaseapp.com",
+    projectId: "fir-comp1800-f0f91",
+    storageBucket: "fir-comp1800-f0f91.appspot.com",
+    messagingSenderId: "735048606677",
+    appId: "1:735048606677:web:acb3570cda8a7b7075935c"
 };
 
 //--------------------------------------------
